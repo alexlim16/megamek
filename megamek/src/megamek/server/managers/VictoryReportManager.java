38a932cf874e08fa1638ffdadf2d4199b9e68ad3
Roman Ouellet-Arijon <romanouellet@gmail.com>
2024-04-07 18:44:12 -0400

    New VictoryReportManager class that extends VictoryManager

---
+++ megamek/src/megamek/server/managers/VictoryReportManager.java
@@ -0,0 +1,9 @@
+package megamek.server.managers;
+
+import megamek.server.GameManager;
+
+public class VictoryReportManager extends VictoryManager {
+    public VictoryReportManager(GameManager gameManager) {
+        super(gameManager);
+    }
+}
